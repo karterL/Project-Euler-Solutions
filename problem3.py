c5e835886c8ed45f8119ce4ca35a88a17d68276f
karterL <cadamloya@gmail.com>
2023-10-20 11:40:54 -0700

    Completed Problem 3!

--- problem3.py
+++ problem3.py
@@ -4,19 +4,18 @@
    What is the largest prime factor of the number
    600851475143?'''
 
-def prime_factor(num, prime):
-    while True:
-        for i in range(2,prime):
-            if (prime%i) == 0:
-                break
-            elif ((prime-1) == i) and ((num%prime) == 0):
-                num = num//prime
-                print(prime)
-                print(num)
-                # print(num)
-        prime += 1
-
 num = 600851475143
 prime = 3
-print(prime_factor(num, prime))
+while True:
+    for i in range(2, prime):
+        if (prime%i) == 0:
+            break
+        elif ((prime-1) == i) and ((num%prime) == 0):
+            num = num//prime
+            print(num, prime)
+    if num == 1:
+        break
+    prime += 1
+print("The largest prime factor is ",prime)
+
 
