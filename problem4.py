5bb03365a63f9d49913f7f4d1139d007c1501200
karterL <cadamloya@gmail.com>
2023-10-21 18:35:00 -0700

    Completed Problem 4! At first I could not figure out how to loop through each value, but after doing research I realized I could loop through every triple digit value in a nested for loop!

--- problem4.py
+++ problem4.py
@@ -6,18 +6,13 @@
    Find the largest palindrome made from the
    product of two 3-digit numbers.'''
 
-import math
-
-def reverse(num):
-   return int(num != 0) and ((num % 10) *
-            (10**int(math.log(num,10))) +
-            reverse(num // 10))
-num1 = 100
-num2 = 100
-test = 999666999
-while num1 != 1000:
-   if (num1 * num2) == reverse(num1 * num2):
-      print(True)
-   if num1 > num2:
-      num2 += 1
-print(reverse(test))
+palindrome = 0
+for i in range(999,100,-1):
+   for j in range(i,100,-1):
+      print(i,' ',j)
+      num = i * j
+      if num > palindrome:
+         snum = str(i * j)
+         if snum == snum[::-1]:
+            palindrome = i * j
+print(palindrome)
